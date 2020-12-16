a475cf64f9eda01709e3ab85ad3b3347cd9658d6
Dave Rayment <dave.rayment@gmail.com>
2020-12-16 01:29:55 +0000

    Day 12 first star

---
+++ Day12/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+string[] Input = File.ReadAllLines("Input.txt");
+
+int north = 0;
+int east = 0;
+char heading = 'E';
+string directions = "NESW";
+
+foreach (string line in Input)
+{
+    int distance = int.Parse(line.Substring(1));
+    char command = line[0];
+    if (command == 'F')
+    {
+        command = heading;
+    }
+    switch (command)
+    {
+        case 'N':
+            north += distance;
+            break;
+        case 'S':
+            north -= distance;
+            break;
+        case 'E':
+            east += distance;
+            break;
+        case 'W':
+            east -= distance;
+            break;
+        case 'R':
+            int rotate = distance / 90;
+            heading = directions[(directions.IndexOf(heading) + rotate) % 4];
+            break;
+        case 'L':
+            rotate = 4 - distance / 90;
+            heading = directions[(directions.IndexOf(heading) + rotate) % 4];
+            break;
+        default:
+            break;
+    }
+}
+
+Console.WriteLine($"Q1: {Math.Abs(north) + Math.Abs(east)}");
