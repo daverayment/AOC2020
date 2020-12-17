c90e4222bdc4608b2b64c0ec569341da8b685732
Dave Rayment <dave.rayment@gmail.com>
2020-12-17 19:45:51 +0000

    Day 13 start

---
+++ Day13/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+string[] Input = File.ReadAllLines("Input.txt");
+int timestamp = int.Parse(Input[0]);
+var buses = Input[1].Split(',').Where(x => x != "x").Select(x => int.Parse(x)).ToList();
+
+int lowestRem = int.MaxValue;
+int lowestBus = 0;
+
+foreach (int bus in buses) 
+{
+    int rem = timestamp % bus;
+    if (rem < lowestRem)
+    {
+        lowestBus = bus;
+        lowestRem = rem;
+    }
+}
+
+Console.WriteLine($"Q1: {lowestBus * lowestRem}");
+
