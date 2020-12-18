5ce5349844dda06bd097c6941aa834a6dd80be92
Dave Rayment <dave.rayment@gmail.com>
2020-12-18 22:36:54 +0000

    Day 13

--- Day13/Program.cs
+++ Day13/Program.cs
@@ -1,23 +1,51 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Collections.Generic;
 
 string[] Input = File.ReadAllLines("Input.txt");
 int timestamp = int.Parse(Input[0]);
 var buses = Input[1].Split(',').Where(x => x != "x").Select(x => int.Parse(x)).ToList();
 
-int lowestRem = int.MaxValue;
-int lowestBus = 0;
+int waitTime = int.MaxValue;
+int bestBus = 0;
 
 foreach (int bus in buses) 
 {
-    int rem = timestamp % bus;
-    if (rem < lowestRem)
+    int departs = bus - (timestamp % bus);
+    if (departs < waitTime)
     {
-        lowestBus = bus;
-        lowestRem = rem;
+        waitTime = departs;
+        bestBus = bus;
     }
 }
 
-Console.WriteLine($"Q1: {lowestBus * lowestRem}");
+Console.WriteLine($"Q1: {waitTime * bestBus}");
 
+var allBuses = Input[1].Split(',').ToArray();
+// Hold bus ID and time offset
+var q2Buses = new Dictionary<uint, uint>();
+for (uint i = 0; i < allBuses.Length; i++)
+{
+    if (allBuses[i] != "x")
+    {
+        q2Buses.Add(uint.Parse(allBuses[i]), i);
+    }
+}
+
+// Check this often for the next bus in the sequence
+ulong skip = q2Buses.First().Key;
+ulong pos = skip;
+
+foreach (var bus in q2Buses.Skip(1))
+{
+    while ((pos + bus.Value) % bus.Key != 0)
+    {
+        pos += skip;
+    }
+    // The positions can only line up in multiples of the previous keys
+    // multipled together.
+    skip *= bus.Key;
+}
+
+Console.WriteLine($"Q2: " + pos);
