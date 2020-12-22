61cf4b90002595dcaeb2cd08bdbcdcad9b02e1a7
Dave Rayment <dave.rayment@gmail.com>
2020-12-22 23:03:48 +0000

    Day 15

---
+++ Day15/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+int[] Input = { 12,1,16,3,11,0 };
+
+Console.WriteLine($"Q1: {Play(2020)}");
+Console.WriteLine($"Q2: {Play(30000000)}");
+
+int Play(int turns)
+{
+    // Number and turn last seen
+    Dictionary<int, int> seen = new();
+    
+    int turn = 1;
+    foreach (int num in Input)
+    {
+        seen.Add(num, turn++);
+    }
+
+    int nextNum = 0;
+    int lastNum;
+    do
+    {
+        lastNum = nextNum;
+
+        if (seen.ContainsKey(nextNum))
+        {
+            int temp = nextNum;
+            nextNum = turn - seen[nextNum];
+            seen[temp] = turn;
+        }
+        else
+        {
+            seen.Add(nextNum, turn);
+            nextNum = 0;
+        }
+        turn++;
+    } while (turn <= turns);
+
+    return lastNum;
+}
